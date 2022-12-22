f2410adc934d9a72054d63db3821419dff6a0bc5
Nur Yurtseven <karakosenur@gmail.com>
2022-12-22 14:12:58 +0300

    v3

---
+++ src/B3.java
@@ -0,0 +1,17 @@
+public class B3 {
+    public static void main(String[] args) {
+
+        /*
+        projemizde onemli bir degisiklik yapmak istiyoruz
+        bu degisiklige 10 gun calisacagiz
+        degisikligi yapmaya devam ederken orjinal projemizi riske atmamak
+        ve eger son kullanici kullaniyorsa olusabilecek sorunları engellemek icin
+
+        orijinal proje uzerinde calismak yerine
+        yeni bir branch olusturup o brancte degisiklikleri yapariz
+
+        calisma sonucunda istenen hedefe ulasabilirsek
+        olusturdugumuz branch'i asil branch'e kopyalariz
+         */
+    }
+}
